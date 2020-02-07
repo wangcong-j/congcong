a4b1abba695f70be9a50fe4aa74aabe2d490fd31
unknown <you@example.com>
2020-02-07 16:13:08 +0800

    使用vue-router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from '@/router'
 Vue.config.productionTip = false
 
 // 根实例
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
 
---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+Vue.use(VueRouter)
+
+// 实例化
+const router = new VueRouter({
+  // 路由配置对象
+  // 关键选项：routes  作用：配置路由规则
+  routes: []
+})
+
+// 导出
+export default router
